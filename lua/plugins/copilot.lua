c1a8982c0b897547b189cb832b45824b97beb710
Scott McAllister <mr.scott.mcallister@gmail.com>
2024-11-18 21:44:53 -0400

    add harpoon and update copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -33,6 +33,14 @@ return {
         copilot_on = not copilot_on
       end, { nargs = 0 })
 
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
       vim.keymap.set("n", "<leader>ct", ":CopilotToggle<CR>", { noremap = true, silent = true })
     end
   },
