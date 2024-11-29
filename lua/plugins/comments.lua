8c8228ec10b22f44a8da2be3be571b3c3108b257
McAllister, Scott <smcallister@cvent.com>
2024-11-29 17:30:22 -0400

    add java support, change to rose-pine, and fix problem with commenting code

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,11 +1,10 @@
 return {
-  "terrortylor/nvim-comment",
-  config = function()
-    require("nvim_comment").setup({
-      comment_empty = false,
-      create_mappings = true
-    })
-    -- Add custom key mapping with description
-    vim.api.nvim_set_keymap('v', '<leader>/', '<cmd>lua require("nvim_comment").comment_toggle()<CR>', { noremap = true, silent = true, desc = "Comment out selected lines" })
-  end
+  'numToStr/Comment.nvim',
+  opts = {
+    opleader = {
+      line = '<leader>/',
+      block = '<leader>b',
+    }
+  }
 }
+
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,22 +6,29 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   {
+    "nvim-java/nvim-java",
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "jdtls",
           "lua_ls",
           "ruby_lsp",
           "tailwindcss",
           "ts_ls"
         }
       })
+      require("java").setup()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      lspconfig.ruby_lsp.setup({})
+      lspconfig.jdtls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show hover' })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
     end
   }
 }
